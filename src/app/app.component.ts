c8f1da61270842ab5b220b36e8f602d92986f063
Hior_Templario <tu_correo@ejemplo.com>
2024-12-23 22:07:59 -0500

    Release 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ProductListComponent } from './components/product-list/product-list.component'
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.less'
 })
 export class AppComponent {
   title = 'angular-ecommerce';
---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
+
+
+
+
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule],
+  //imports: [],
+  //templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+
+
+export class ProductListComponent implements OnInit{
+
+  products: Product[] = [];
+
+ constructor(private productService:ProductService) { }
+
+  ngOnInit(): void {
+    this.listProducts();
+
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+
+        
+      }
+    )
+  }
+}
